6056bd95069ef9fba882696e943abc8d2e0e272a
Ahmed101Mohammed <ahmedmohamedal3adl@gmail.com>
2024-06-26 11:03:39 +0300

    Change the application initial data.

--- initData.js
+++ initData.js
@@ -1,15 +1,19 @@
 let introductionAppToDoList = {date: "2024-06-25T20:50:30.424Z",
     description: "Checklists to check if you know all ToDoly features",
     items: [
-        {content: 'Create a new list with any unique name.', isDone: false, parentIdFormat: 'discovering-todoly-application'}
+        {content: 'Create a new list with any unique name. (Note: you can\'t create two lists with the same name)', isDone: false, parentIdFormat: 'discovering-todoly-application'}
         , 
         {content: 'Click on your To Do List title to open the list page.', isDone: false, parentIdFormat: 'discovering-todoly-application'}
         , 
         {content: "Add any number of items (Note: you can't create two items with the same content)", isDone: false, parentIdFormat: 'discovering-todoly-application'}
         ,
-        {content: 'Click on the checkbox of one of your list items, r…e home page, and refresh it to see your progress.', isDone: false, parentIdFormat: 'discovering-todoly-application'}
+        {content: 'Click on the checkbox of one of your list items, and go back to home page, and refresh it to see your progress.', isDone: false, parentIdFormat: 'discovering-todoly-application'}
         ,
-        {content: 'Go back to your To Do List page, and delete on of the items.', isDone: false, parentIdFormat: 'discovering-todoly-application'}
+        {content: "Change the periority of one of your list items, by click on up or down arrow", isDone: false, parentIdFormat: 'discovering-todoly-application'}
+        ,
+        {content: 'Delete one of the items.', isDone: false, parentIdFormat: 'discovering-todoly-application'}
+        ,
+        {content: 'Change the periority of your To Do List, to have the 1st periority. (by click on up arrow)', isDone: false, parentIdFormat: 'discovering-todoly-application'}
         ,
         {content: 'Delete "Discovering TODOly application" To Do List, now you are ready to use all ToDoly features.', isDone: false, parentIdFormat: 'discovering-todoly-application'}
     ],
