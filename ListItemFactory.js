6f12b73c91ca5fc2c20bb82b51e56494c8a9b475
Ahmed101Mohammed <ahmedmohamedal3adl@gmail.com>
2024-06-27 12:14:23 +0300

    Fixing check list item functionality.

--- ListItemFactory.js
+++ ListItemFactory.js
@@ -1,153 +1,154 @@
 const ListItem = (content, isDone, parentIdFormat)=>
-{
-    return {
-        content,
-        isDone,
-        parentIdFormat,
-        htmlCode()
-        {
-            return `
-                <li class="${parentIdFormat}"> 
-                    <div class="body">
-                        <div class = "checkbox">
+    {
+        return {
+            content,
+            isDone,
+            parentIdFormat,
+            htmlCode()
+            {
+                return `
+                    <li class="${parentIdFormat}"> 
+                        <div class="body">
                             <input type="checkbox" ${this.addCheckedAttribute()}>
+                            <p>${content}<p>
+                        </div> 
+                        <div class="sittings">
+                            <div class="up-down">
+                                <span class="material-symbols-outlined item-up">
+                                    keyboard_arrow_up
+                                </span>
+                                <span class="material-symbols-outlined item-down">
+                                    keyboard_arrow_down
+                                </span>
+                            </div>
+                            <div class="close">
+                                <span class="material-symbols-outlined delete-item delete-item">close</span>
+                            </div>
                         </div>
-                        <p>${content}<p>
-                    </div> 
-                    <div class="sittings">
-                        <div class="up-down">
-                            <span class="material-symbols-outlined item-up">
-                                keyboard_arrow_up
-                            </span>
-                            <span class="material-symbols-outlined item-down">
-                                keyboard_arrow_down
-                            </span>
-                        </div>
-                        <div class="close">
-                            <span class="material-symbols-outlined delete-item delete-item">close</span>
-                        </div>
-                    </div>
-                </li>
-            `
-        },
-        addCheckedAttribute()
-        {
-            if(isDone)
+                    </li>
+                `
+            },
+            addCheckedAttribute()
             {
-                return "checked";
-            }
-            return "";
-        },
-        addToDom()
-        {
-            const ul = document.querySelector("ul");
-            ul.insertAdjacentHTML("beforeend", this.htmlCode());
-        },
-        addListItemToDBAndDOM()
-        {
-            const targetList = JSON.parse(localStorage.getItem("lists")).find(list =>
+                if(isDone)
                 {
-                    const listObject = ToDoList(list.title, list.description, list.date, ...list.items);
-                    return listObject.convertTitleToDashedLowerCase() === parentIdFormat;
+                    return "checked";
                 }
-            );
-
-            const listItem = targetList.items.find(item => item.content === this.content);
-            if(listItem){return false};
-           
-            targetList.items.push({content: this.content, isDone: this.isDone, parentIdFormat: this.parentIdFormat});
-            let newLists = JSON.parse(localStorage.getItem("lists")).filter(list => list.title !== targetList.title);
-            newLists.push(targetList);
-
-            localStorage.setItem("lists", JSON.stringify(newLists));
-
-            this.addToDom();
-            return true;
-        },
-        deleteFromDBAndDom()
-        {
-            const targetList = JSON.parse(localStorage.getItem("lists")).find(list => 
+                return "";
+            },
+            addToDom()
             {
-                const listObject = ToDoList(list.title, list.description, list.date, ...list.items);
-                return listObject.convertTitleToDashedLowerCase() === this.parentIdFormat
-            });
-            targetList.items = targetList.items.filter(item => item.content !== this.content);
-            let newLists = JSON.parse(localStorage.getItem("lists")).filter(list => list.title !== targetList.title);
-            newLists.push(targetList);
-            localStorage.setItem("lists", JSON.stringify(newLists));
-
-            this.deleteListItemFromDom();
-        },
-        deleteListItemFromDom()
-        {
-            const listItems = document.querySelectorAll(`.${this.parentIdFormat}`);
-            for(let item of listItems)
+                const ul = document.querySelector("ul");
+                ul.insertAdjacentHTML("beforeend", this.htmlCode());
+            },
+            addListItemToDBAndDOM()
             {
-                if(item.querySelector(".body p").textContent === this.content)
-                {
-                    item.remove();
-                }
-            }
-        },
-        updateInDB()
-        {
-            const targetList = JSON.parse(localStorage.getItem("lists")).find(list => 
+                const targetList = JSON.parse(localStorage.getItem("lists")).find(list =>
+                    {
+                        const listObject = ToDoList(list.title, list.description, list.date, ...list.items);
+                        return listObject.convertTitleToDashedLowerCase() === parentIdFormat;
+                    }
+                );
+    
+                const listItem = targetList.items.find(item => item.content === this.content);
+                if(listItem){return false};
+               
+                targetList.items.push({content: this.content, isDone: this.isDone, parentIdFormat: this.parentIdFormat});
+                let newLists = JSON.parse(localStorage.getItem("lists")).filter(list => list.title !== targetList.title);
+                newLists.push(targetList);
+    
+                localStorage.setItem("lists", JSON.stringify(newLists));
+    
+                this.addToDom();
+                return true;
+            },
+            deleteFromDBAndDom()
             {
-                const listObject = ToDoList(list.title, list.description, list.date, ...list.items);
-                return listObject.convertTitleToDashedLowerCase() === this.parentIdFormat
-            });
-            targetList.items = targetList.items.filter(item => item.content !== this.content);
-            targetList.items.push({content, isDone, parentIdFormat});
-            let newLists = JSON.parse(localStorage.getItem("lists")).filter(list => list.title !== targetList.title);
-            newLists.push(targetList);
-            localStorage.setItem("lists", JSON.stringify(newLists));
-        },
-        priorityUp()
-        {
-            const lists = JSON.parse(localStorage.getItem("lists"));
-            const listItems = lists.find(list =>
+                const targetList = JSON.parse(localStorage.getItem("lists")).find(list => 
                 {
                     const listObject = ToDoList(list.title, list.description, list.date, ...list.items);
                     return listObject.convertTitleToDashedLowerCase() === this.parentIdFormat
-                }
-            );
-
-            const listItem = listItems.items.find(item => item.content === this.content);
-            const listItemIndex = listItems.items.indexOf(listItem);
-            if(listItemIndex > 0)
+                });
+                targetList.items = targetList.items.filter(item => item.content !== this.content);
+                let newLists = JSON.parse(localStorage.getItem("lists")).filter(list => list.title !== targetList.title);
+                newLists.push(targetList);
+                localStorage.setItem("lists", JSON.stringify(newLists));
+    
+                this.deleteListItemFromDom();
+            },
+            deleteListItemFromDom()
             {
-                // swap list-items in DB
-                listItems.items.splice(listItemIndex, 1);
-                listItems.items.splice(listItemIndex - 1, 0, listItem);
-                localStorage.setItem("lists", JSON.stringify(lists));
-
-                // rerender dom
-                window.location.reload();
-            }
-        },
-        priorityDown()
-        {
-            const lists = JSON.parse(localStorage.getItem("lists"));
-            const listItems = lists.find(list =>
+                const listItems = document.querySelectorAll(`.${this.parentIdFormat}`);
+                for(let item of listItems)
+                {
+                    if(item.querySelector(".body p").textContent === this.content)
+                    {
+                        item.remove();
+                    }
+                }
+            },
+            updateInDB()
+            {
+                const targetList = JSON.parse(localStorage.getItem("lists")).find(list => 
                 {
                     const listObject = ToDoList(list.title, list.description, list.date, ...list.items);
                     return listObject.convertTitleToDashedLowerCase() === this.parentIdFormat
-                }
-            );
-
-            const listItem = listItems.items.find(item => item.content === this.content);
-            const listItemIndex = listItems.items.indexOf(listItem);
-            if(listItemIndex < listItems.items.length - 1)
+                });
+                let items = targetList.items;
+                let item = items.find(item => item.content === this.content);
+                item.isDone = this.isDone;
+                item.content = this.content;
+                item.parentIdFormat = this.parentIdFormat;
+                let newLists = JSON.parse(localStorage.getItem("lists")).filter(list => list.title !== targetList.title);
+                newLists.push(targetList);
+                localStorage.setItem("lists", JSON.stringify(newLists));
+            },
+            priorityUp()
             {
-                // swap list-items in DB
-                listItems.items.splice(listItemIndex, 1);
-                listItems.items.splice(listItemIndex + 1, 0, listItem);
-                localStorage.setItem("lists", JSON.stringify(lists));
-
-                // rerender dom
-                window.location.reload();
+                const lists = JSON.parse(localStorage.getItem("lists"));
+                const listItems = lists.find(list =>
+                    {
+                        const listObject = ToDoList(list.title, list.description, list.date, ...list.items);
+                        return listObject.convertTitleToDashedLowerCase() === this.parentIdFormat
+                    }
+                );
+    
+                const listItem = listItems.items.find(item => item.content === this.content);
+                const listItemIndex = listItems.items.indexOf(listItem);
+                if(listItemIndex > 0)
+                {
+                    // swap list-items in DB
+                    listItems.items.splice(listItemIndex, 1);
+                    listItems.items.splice(listItemIndex - 1, 0, listItem);
+                    localStorage.setItem("lists", JSON.stringify(lists));
+    
+                    // rerender dom
+                    window.location.reload();
+                }
+            },
+            priorityDown()
+            {
+                const lists = JSON.parse(localStorage.getItem("lists"));
+                const listItems = lists.find(list =>
+                    {
+                        const listObject = ToDoList(list.title, list.description, list.date, ...list.items);
+                        return listObject.convertTitleToDashedLowerCase() === this.parentIdFormat
+                    }
+                );
+    
+                const listItem = listItems.items.find(item => item.content === this.content);
+                const listItemIndex = listItems.items.indexOf(listItem);
+                if(listItemIndex < listItems.items.length - 1)
+                {
+                    // swap list-items in DB
+                    listItems.items.splice(listItemIndex, 1);
+                    listItems.items.splice(listItemIndex + 1, 0, listItem);
+                    localStorage.setItem("lists", JSON.stringify(lists));
+    
+                    // rerender dom
+                    window.location.reload();
+                }
+            
             }
-        
         }
     }
-}
